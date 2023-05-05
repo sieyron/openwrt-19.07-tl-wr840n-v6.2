7a67ff210f30d4db899154ecaa463d5c5708be06
Sirun <60006921+sieyron@users.noreply.github.com>
2023-05-05 18:11:42 +0700

    ramips: add support for TP-Link TL-WR840N v6.2

--- target/linux/ramips/image/mt76x8.mk
+++ target/linux/ramips/image/mt76x8.mk
@@ -247,6 +247,23 @@ define Device/tl-wr840n-v5
 endef
 TARGET_DEVICES += tl-wr840n-v5
 
+define Device/tl-wr840n-v6.2
+  $(Device/tplink)
+  DTS := TL-WR840NV6.2
+  IMAGE_SIZE := 3968k
+  DEVICE_TITLE := TP-Link TL-WR840N v6.2
+  TPLINK_FLASHLAYOUT := 4Mmtk
+  TPLINK_HWID := 0x08400006
+  TPLINK_HWREV := 0x1
+  TPLINK_HWREVADD := 0x7
+  TPLINK_HVERSION := 3
+  SUPPORTED_DEVICES += tplink,tl-wr840n-v6.2
+  DEFAULT := n
+  IMAGES := sysupgrade.bin tftp-recovery.bin
+  IMAGE/tftp-recovery.bin := pad-extra 64k | $$(IMAGE/factory.bin)
+endef
+TARGET_DEVICES += tl-wr840n-v6.2
+
 define Device/tl-wr841n-v13
   $(Device/tplink)
   DTS := TL-WR841NV13
